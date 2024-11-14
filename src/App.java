4a5c4a5b74b6fbec9f86fb4375801cb2f84a949d
nsilver9 <28nsilver@gannacademy.org>
2024-11-14 13:36:14 -0500

    Not finished

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@
  *
  * This method:
  *  -takes in an array of integers that will have a length. 
- *  -If the array has a length of 2 or more, return the sum of the first values
+ *  -If the array has a length of 2 or more, return the sum of the first two values
  *  -If the array has a length of 1, just return that value
  *  -If the array has a length of 0, return 0.
  *  
@@ -23,11 +23,14 @@
  * An example of this method working is included in the main method 
 */
 
+import java.util.Scanner;
+
 public class App {
+    
     public static void main(String[] args)  {
         //firstTwoSum example
         System.out.println("With input [1, 2, 3], you should get: 3. You got: " + firstTwoSum(new int[]{1,2,3}));
-        System.out.println("With input [5, 6, 9, 2], you should get: 11. You got: " + firstTwoSum(new int[]{1,2,3}));
+        System.out.println("With input [5, 6, 9, 2], you should get: 11. You got: " + firstTwoSum(new int[]{5,6,9,2}));
         System.out.println("With input [7], you should get: 7. You got: " + firstTwoSum(new int[]{7}));
         System.out.println("With input [], you should get: 0. You got: " + firstTwoSum(new int[]{}));
         
@@ -40,14 +43,24 @@ public class App {
     }
 
     public static int firstTwoSum(int[] nums) {
-
-        return -1; //this is a placeholder
-
+        int sumOfFirst2 = nums[0] + nums[1];
+        if (nums.length >= 2) {
+            return sumOfFirst2;
+        } else if (nums.length == 1) {
+            return nums[0];
+        } else {
+            return 0;
+        }
     }
 
-    public static int minWordLength(String sentence){
-
-
-        return -1; //this is a placeholder
+    public static int minWordLength(String sentence) {
+        String[] words = sentence.split(" ");
+        String shortest = words[0];
+        for(String s: words) {
+            if (sentence.equals("")) {
+                break;
+        }
+            if (shortest.length() > s)
+            
     }
 }
